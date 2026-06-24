a94060338839f2ea51f2a8e9a259256785e1c940
Andrey <a.r.stdnk@gmail.com>
2026-06-24 13:36:17 +0300

    Update 1_task.sql

--- 1_task.sql
+++ 1_task.sql
@@ -2,7 +2,7 @@ SELECT s.sailor_id, s.full_name
 FROM sailors s
 JOIN logs l
 	ON s.sailor_id = l.captain_id --если кок не может быть капитаном, делаем JOIN только по first_officer_id
-  OR s.sailor_id = l.first_officer_id 
+  	OR s.sailor_id = l.first_officer_id 
 WHERE s.rank = 'Кок'
 	AND l.port = 'Восточная гавань'
 	AND EXTRACT(MONTH FROM l.sail_date) = 11
