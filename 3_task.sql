3aa791e8b6eb2ba7ecf54bb63c8ebc984e8d4bab
Andrey <a.r.stdnk@gmail.com>
2026-06-24 13:41:45 +0300

    Update 3_task.sql

--- 3_task.sql
+++ 3_task.sql
@@ -4,7 +4,7 @@ LEFT JOIN logs l --LEFT JOIN чтобы включить капитанов с 0
 	ON s.sailor_id = l.captain_id
 	AND l.ship_id IN (SELECT ship_id FROM ships WHERE type_flag = 1)
   	AND EXTRACT(YEAR FROM l.sail_date) = EXTRACT(YEAR FROM CURRENT_DATE)
-WHERE s.rank = 'Капитан'
+WHERE s.rank = 'Капитан' --фильтрую по должности из таблицы sailors
 GROUP BY s.sailor_id, s.full_name
 ORDER BY cargo_trips ASC
 LIMIT 5;
